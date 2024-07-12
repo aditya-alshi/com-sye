2c1f0df4926eef31084e0d4419ab3ff75779cf1f
Aditya Alshi <alshiaditya55@gmail.com>
2024-07-13 02:54:39 +0530

    cleared prisma

---
+++ prisma/migrations/20240712211619_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `City` (
+    `cityId` VARCHAR(191) NOT NULL,
+    `cityName` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`cityId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
