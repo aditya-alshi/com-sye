4238090be9710ac4f8ff1a77cc77ec85b1ba11c2
Aditya Alshi <alshiaditya55@gmail.com>
2024-07-14 19:23:06 +0530

    added prisma and postgres for vercel deployment

--- prisma/migrations/20240712211619_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `City` (
-    `cityId` VARCHAR(191) NOT NULL,
-    `cityName` VARCHAR(255) NOT NULL,
-
-    PRIMARY KEY (`cityId`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/migrations/20240714135013_postgresnew/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "City" (
+    "city_id" TEXT NOT NULL,
+    "city_name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "City_pkey" PRIMARY KEY ("city_id")
+);
